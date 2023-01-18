46dec3ec704b93fa31c65d8ccdbbb4064d6b296f
sabernstein <sadiebernstein2023@u.northwestern.edu>
2023-01-18 14:51:19 -0600

    low pass on all 6 cells and their 2 averages

--- buffer.py
+++ buffer.py
@@ -22,7 +22,9 @@ for i in range(0, len(v2)):
 
 for i in range(0, len(v2_volt)):
     if v2_volt[i] < 0:
-        v2_volt[i] = 0
+        v2_volt[i] = v2_volt[i]
+    if v2_volt[i] < -200:
+         v2_volt[i] = (v2_volt[i-1]+v2_volt[i+1])/2
 
 v2_power = []
 for v in v2_volt:
@@ -31,6 +33,34 @@ for v in v2_volt:
 x2 = columns['unix_time']
 d0 = datetime.fromtimestamp(int(x2[0]))
 days = []
+
+
+v1 = columns['Vertical 1']
+v3 = columns['Vertical 3']
+p1 = columns['Planar 1']
+p2 = columns['Planar 2']
+p3 = columns['Planar 3']
+
+vertical_ave = []
+
+planar_ave = []
+v1_power = []
+v3_power = []
+p1_power = []
+p2_power = []
+p3_power = []
+for i in range(0, len(v3)):
+    v1_power.append(float(v1[i]))
+    v3_power.append(float(v3[i]))
+
+    p1_power.append(float(p1[i]))
+    p2_power.append(float(p2[i]))
+    p3_power.append(float(p3[i]))
+
+
+for i in range(0, len(v3_power)):
+    vertical_ave.append((v1_power[i] + v2_power[i] + v3_power[i])/3)
+    planar_ave.append((p1_power[i] + p2_power[i] + p3_power[i])/3)
 
 for d in x2:
     day = datetime.fromtimestamp(int(d))
@@ -47,6 +77,7 @@ def butter_lowpass_filter(data, cutoff, fs, order=5):
     return y
 
 
+
 # Filter requirements.
 order = 6
 fs = 1/60      # sample rate, Hz
@@ -54,17 +85,6 @@ cutoff = 1/(12*3600)  # desired cutoff frequency of the filter, Hz
 
 # Get the filter coefficients so we can check its frequency response.
 b, a = butter_lowpass(cutoff, fs, order)
-
-# Plot the frequency response.
-# w, h = freqz(b, a, fs=fs, worN=8000)
-# plt.subplot(2, 1, 1)
-# plt.plot(w, np.abs(h), 'b')
-# plt.plot(cutoff, 0.5*np.sqrt(2), 'ko')
-# plt.axvline(cutoff, color='k')
-# plt.xlim(0, 0.5*fs)
-# plt.title("Lowpass Filter Frequency Response")
-# plt.xlabel('Frequency [Hz]')
-# plt.grid()
 
 
 # Demonstrate the use of the filter.
@@ -76,36 +96,51 @@ t = np.linspace(0, T, n, endpoint=False)
 data = np.sin(1.2*2*np.pi*t) + 1.5*np.cos(9*2*np.pi*t) + 0.5*np.sin(12.0*2*np.pi*t)
 # new_v2 = np.array(v2_power)
 # Filter the data, and plot both the original and filtered signals.
-y = butter_lowpass_filter(v2_power, cutoff, fs, order)
+y1 = butter_lowpass_filter(v1_power, cutoff, fs, order)
+y2 = butter_lowpass_filter(v2_power, cutoff, fs, order)
+y3 = butter_lowpass_filter(v3_power, cutoff, fs, order)
+y4 = butter_lowpass_filter(p1_power, cutoff, fs, order)
+y5 = butter_lowpass_filter(p2_power, cutoff, fs, order)
+y6 = butter_lowpass_filter(p3_power, cutoff, fs, order)
+v_ave = butter_lowpass_filter(vertical_ave, cutoff, fs, order)
+p_ave = butter_lowpass_filter(planar_ave, cutoff, fs, order)
+
+# plt.plot(days[:105102], v2_power[:105102], 'b-', label='data')
+# plt.plot(days[:105102], y[:105102], 'g-', linewidth=2, label='filtered data')
 
 
-plt.plot(days[:105102], v2_power[:105102], 'b-', label='data')
-plt.plot(days[:105102], y[:105102], 'g-', linewidth=2, label='filtered data')
-plt.xlabel('Time [sec]')
-# plt.ylim(top=250, bottom=0)
-plt.grid()
+
+# # plot line
+plt.plot(days[:105102], v_ave[:105102], label = "Vertical Average", alpha = 1, color= (0,0.1,1))
+plt.plot(days[:105102], y1[:105102], label = "Vertical 1", alpha = 0.2, color= (0,0.2,0.5))
+# plt.plot(days[:105102], v2_power[:105102], label = "Vertical 2", alpha = 0.2, color= (0,0.4,0.5))
+plt.plot(days[:105102], y2[:105102], label='Vertical 2', alpha = 0.2, color= (0,0.4,0.5))
+plt.plot(days[:105102], y3[:105102], label = "Vertical 3", alpha = 0.2, color= (0,0.6,0.5))
+
+plt.plot(days[:105102], p_ave[:105102], label = "Horizontal Average", alpha = 1, color= (1,0.1,0))
+plt.plot(days[:105102], y4[:105102], label = "Horizontal 1", alpha = 0.2, color= (0.5,0.2,0))
+plt.plot(days[:105102], y5[:105102], label = "Horizontal 2", alpha = 0.2, color= (0.5,0.4,0))
+plt.plot(days[:105102], y6[:105102], label = "Horizontal 3", alpha = 0.2, color= (0.5,0.6,0))
+plt.legend("upper left")
+#rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
+# rc('font',**{'family':'serif','serif':['Times']})
+# rc('text', usetex=True)
+
+#example of vertical line plotting
+# ax.axvspan(8, 14, alpha=0.5, color='red')
+# started drying cells'
+plt.axvline(x = 50.153796, alpha = 0.2, color = 'yellow')
+# label = 'reflooded cells'
+plt.axvspan(50, 58.886458, color = 'yellow', alpha = 0.2)
+# label = 'started drying again'
+plt.axvspan(74.551516, 85, color = 'yellow', alpha = 0.2)
+
+## PROBLEMS!
+# 1. We don't need the lines to show up in legend
+# 2. We want to label or even maybe shade in regions on the graphs (e.g. between the vertical lines)
+
+plt.ylabel('Power (µW)')
+plt.xlabel('Timeline (Days)')
+
 plt.legend()
-
-plt.subplots_adjust(hspace=0.35)
 plt.show()
-
-'''# Demonstrate the use of the filter.
-# First make some data to be filtered.
-T = 5.0         # seconds
-n = int(T * fs) # total number of samples
-t = np.linspace(0, T, n, endpoint=False)
-# "Noisy" data.  We want to recover the 1.2 Hz signal from this.
-data = np.sin(1.2*2*np.pi*t) + 1.5*np.cos(9*2*np.pi*t) + 0.5*np.sin(12.0*2*np.pi*t)
-
-# Filter the data, and plot both the original and filtered signals.
-y = butter_lowpass_filter(data, cutoff, fs, order)
-
-plt.subplot(2, 1, 2)
-plt.plot(t, data, 'b-', label='data')
-plt.plot(t, y, 'g-', linewidth=2, label='filtered data')
-plt.xlabel('Time [sec]')
-plt.grid()
-plt.legend()
-
-plt.subplots_adjust(hspace=0.35)
-plt.show()'''
