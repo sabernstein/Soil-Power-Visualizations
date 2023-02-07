24060c7bec5d1b6481527e5c57e6bae0132f1b49
billyen33 <billmembers33@gmail.com>
2023-02-07 14:35:07 -0600

    commit before pull

--- Runtime_Simulation/models.py
+++ Runtime_Simulation/models.py
@@ -1,3 +1,9 @@
+def internal_R_v3(): #return internal resistance of v3 cells in ohms
+    return (1000 + 1000 + 1000)/3
+
+def internal_R_v0(): #return internal resistance of v0 cells in ohms
+    return (1000 + 1000 + 1000)/3
+
 def SMFC_current(v, R):
     return v/R
 
@@ -5,9 +11,13 @@ def cap_leakage(insul_R=1000, rated_V=4):
     return rated_V/insul_R
 
 def update_capVoltage(v0, V_applied, R, C, dt):
+    #equation source: https://electronics.stackexchange.com/questions/264180/deriving-the-formula-from-scratch-for-charging-a-capacitor
     import math
     #assume constant current in this timestep
-    V_new = V_applied * (1-math.exp(-dt/(R*C)))
+    if V_applied > v0: #only charge if applied voltage is greater than cap voltage
+        V_new = V_applied * (1-math.exp(-dt/(R*C)))
+    else:
+        V_new = v0 #assume we won't discharge if voltage is lower
     return V_new
 
 def update_capEnergy(v0, V_applied, R, C, dt):
