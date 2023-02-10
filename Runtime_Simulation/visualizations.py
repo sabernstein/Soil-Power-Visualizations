2f7d8229e7783451bdfc4a600c3f1ecf92e2abc5
billyen33 <billmembers33@gmail.com>
2023-02-10 14:56:51 -0600

    push current code

--- Runtime_Simulation/visualizations.py
+++ Runtime_Simulation/visualizations.py
@@ -45,7 +45,81 @@ def bar_subplots(data1, data2, data3, data4, data5, data6):
     #axs[2].ticklabel_format(style='plain')
     #axs[2].yaxis.set_major_formatter(mtick.FormatStrFormatter('%.2e'))
     # Put a legend below current axis
-    plt.legend(loc='upper center', bbox_to_anchor=(0.5, 2.4),
+    plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.1),
+          fancybox=True, ncol=4, prop=my_font, frameon=False)
+
+    # axs.flat[0].set(ylabel="Ambiq")
+    axs.set_ylabel("Number of Operations", fontproperties=my_font, size=22)
+    #axs.flat[1].set_ylabel("v3", fontproperties=my_font2)
+    #axs.flat[2].set_ylabel("MARS", fontproperties=my_font2)
+
+
+    #set font type of tickmarks
+    for label in axs.get_xticklabels():
+        label.set_fontproperties(my_font2)
+    for label in axs.get_yticklabels():
+        label.set_fontproperties(my_font2)
+
+    #set font type of tickmarks
+    #for label in axs[1].get_xticklabels():
+    #    label.set_fontproperties(my_font2)
+    #for label in axs[1].get_yticklabels():
+    #    label.set_fontproperties(my_font2)
+
+    #set font type of tickmarks
+    # for label in axs[2].get_xticklabels():
+    #     label.set_fontproperties(my_font2)
+    # for label in axs[2].get_yticklabels():
+    #     label.set_fontproperties(my_font2)
+
+    plt.subplots_adjust(left=0.15, right=0.9, top=0.9, bottom=0.1)
+    #fig.text(0.07, 0.5, "Number of Operations", ha='center', va='center', rotation='vertical', fontproperties=my_font, size=22)
+    # plt.ylabel("Number of Sensor Readings", fontproperties=my_font, size=22)
+    plt.xlabel("Timeline (Days)", fontproperties=my_font, size=22)
+    
+    # plt.legend()
+    plt.show()
+
+
+def bar_subplots2(data1, data2):
+    #import custom font
+    font_path = './font/linux_libertine/LinLibertine_RB.ttf'  # the location of the font file
+    my_font = font_manager.FontProperties(fname=font_path, size=16)  # get the font based on the font_path, set font size
+
+    font_path2 = './font/linux_libertine/LinLibertine_R.ttf'  # the location of the font file
+    my_font2 = font_manager.FontProperties(fname=font_path2, size=20)  # get the font based on the font_path, set font size
+    
+    
+    fig, axs = plt.subplots(1, 1, figsize=(12, 4), sharex=True)
+    barWidth = 0.5
+    x = np.arange(len(data1))
+    x1 = [item + barWidth for item in x]
+    x2 = [item + barWidth for item in x1]
+    axs.bar(x, data1, color="blue", label="v0", width = barWidth, alpha=1)
+    axs.bar(x1, data2, color="green", label="v3", width = barWidth, alpha=1)
+    #axs.set_yscale('log')
+
+    #axs[1].bar(x, data4, color="blue", label="Apollo4", width = barWidth, alpha=1)
+    #axs[1].bar(x1, data5, color="green", label="MSP430", width = barWidth, alpha=1)
+    #axs[1].bar(x2, data6, color="orange", label="MARS", width = barWidth, alpha=1)
+    #axs[1].set_yscale('log')
+
+    #axs[2].bar(x, data3, color="blue", label="v0", width = barWidth, alpha=1)
+    #axs[2].bar(x1, data6, color="green", label="v3", width = barWidth, alpha=0.7)
+    
+    axs.get_yaxis().get_major_formatter().set_scientific(True)
+    #axs[1].get_yaxis().get_major_formatter().set_scientific(True)
+    #axs[2].get_yaxis().get_major_formatter().set_scientific(True)
+
+    plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+    axs.ticklabel_format(style='plain')
+    axs.yaxis.set_major_formatter(mtick.FormatStrFormatter('%.2e'))
+    #axs[1].ticklabel_format(style='plain')
+    #axs[1].yaxis.set_major_formatter(mtick.FormatStrFormatter('%.2e'))
+    #axs[2].ticklabel_format(style='plain')
+    #axs[2].yaxis.set_major_formatter(mtick.FormatStrFormatter('%.2e'))
+    # Put a legend below current axis
+    plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.1),
           fancybox=True, ncol=4, prop=my_font, frameon=False)
 
     # axs.flat[0].set(ylabel="Ambiq")
@@ -103,13 +177,13 @@ def count_vs_cap(cap_list, data1, data2, data3, data4, data5, data6):
 
     fig, axs = plt.subplots(2, 1, figsize=(12, 4), sharex=True)
     axs[0].set_xscale('log')
-    axs[0].plot(cap_list, data1, color="blue", label="Ambiq", alpha=1)
+    axs[0].plot(cap_list, data1, color="blue", label="Apollo4", alpha=1)
     axs[0].plot(cap_list, data2, color="green", label="MSP430", alpha=1)
     axs[0].plot(cap_list, data3, color="orange", label="MARS", alpha=1)
     axs[0].set_yscale('log')
 
     axs[1].set_xscale('log')
-    axs[1].plot(cap_list, data4, color="blue", label="Ambiq", alpha=1)
+    axs[1].plot(cap_list, data4, color="blue", label="Apollo4", alpha=1)
     axs[1].plot(cap_list, data5, color="green", label="MSP430", alpha=1)
     axs[1].plot(cap_list, data6, color="orange", label="MARS", alpha=1)
     axs[1].set_yscale('log')
